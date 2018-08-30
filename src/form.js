3541fbb3dc20891ee0292b4e8c2ec8f2380ec10f
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-30 22:01:48 +0000

    adding items to list

--- src/form.js
+++ src/form.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 
 class Form extends Component{
+  constructor(props){
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+
+  }
   render(){
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className="form-group">
-            <input className="form-control" ref="text" type="text" />
+            <input className="form-control" ref="newItem" type="text" />
           </div>
           <div className="form-group">
-            <button type="submit" className="btn btn-primary btn-lng btn-block">Add New item</button>
+            <button type="submit" id="btn" className="btn btn-primary btn-lng btn-block">Add New item</button>
           </div>
         </form>
       </div>
     )
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+    var newItem = this.refs.newItem.value.trim();
+    if(!newItem){
+      alert('please enter a new item')
+      return
+    }
+    this.props.addNew(newItem)
+    this.refs.newItem.value = '';
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,28 +4,102 @@ import './index.css';
 import Form from './form.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list: [
+        {
+          id: 1,
+          item: 'Apples'
+        },
+        {
+          id: 2,
+          item: 'Pears'
+        },
+        {
+          id: 3,
+          item: 'Bananas'
+        }
+      ], // list array end
+      text: 'HELLO WORLD',
+      style: 'empty',
+      jumboClass: 'jumbotron text-center',
+      darkTheme: false
+    } // this.state end
+
+    this.changeText = this.changeText.bind(this)
+    this.addNewItemToList = this.addNewItemToList.bind(this)
+  } // constructor end
+
   render(){
     return (
       <div>
-        <div className="jumbotron text-center">
-          <h1 className="display-4">Shopping List</h1>
-          <ShoppingList/>
+        <div className={this.state.jumboClass}>
+          <h1 className={this.state.style}>Shopping List</h1>
+          <h3>{this.state.text}</h3>
+          <ShoppingList list={this.state.list}/>
           <hr />
 
-          <Form></Form>
+          <Form addNew={this.addNewItemToList}></Form>
+          <button onClick={this.changeText}>Change state of H3</button>
+          <button onClick={this.changeStyle}>Change style</button>
         </div>
       </div>
     )
+  } // render ends
+
+  changeText(e) {
+    e.preventDefault();
+    console.log(this.state);
+    
+    // alert('h3 will be changed')
+    this.setState({
+      text: 'Button has been clicked',
+      jumboClass: 'jumbotron text-center jumboDark'
+      // darkTheme: !this.state.darkTheme
+    })
+    console.log(this.state);
+
+
+    // if(this.state.darkTheme === true){
+    //   this.setState({
+    //     jumboClass: 'jumbotron text-center jumboDark'
+    //   })
+    // } else {
+    //   this.setState({
+    //     jumboClass: 'jumbotron text-center'
+    //   })
+    // }
+      
+  } // end change text
+
+  addNewItemToList(item) {
+    var newItem = {
+      id: this.state.list.length + 1,
+      item: item
+    }
+
+    this.setState({list: this.state.list.concat(newItem)} )
   }
-}
+
+  changeStyle(e) {
+    e.preventDefault();
+    this.setState({style: 'display-4'})
+  }
+} // component ends
         
 
 class ShoppingList extends Component {
   render(){
     return (
       <div>
-        <ul class="list-group">
-          <li class="list-group-item">Cras justo odio</li>
+        <ul className="list-group">
+          {
+            this.props.list.map(product => {
+              return <li key={product.id} product={product} className="list-group-item">{product.item}</li>
+            })
+          }
+          
         </ul>
       </div>
     )
